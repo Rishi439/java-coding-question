1d120bf5b5f8058d8f8e19a9dfd3379ed2ea7f14
Rishiraj Sharma <rishiraj.sharma@neosofttech.com>
2019-05-22 19:58:29 +0530

    Changes in sorting implementation

---
+++ src/com/programming/sorting/BinaryInsertionSort.java
@@ -0,0 +1,21 @@
+package com.programming.sorting;
+
+public class BinaryInsertionSort extends Sorting {
+
+	public BinaryInsertionSort(int[] arr) {
+		super(arr, "Binary Insertion Sort");
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int[] sort() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private void binaryInsertionSort() {
+		
+	}
+	
+
+}
--- src/com/programming/sorting/SortingDemo.java
+++ src/com/programming/sorting/SortingDemo.java
@@ -14,61 +14,18 @@ public class SortingDemo {
 		System.out.println("Number of items in array : ");
 		int sizeOfArray = scanner.nextInt();
 		Random random = new Random();
-		int[] unsortedArray = new int[sizeOfArray];
-		int[] unsortedArray1 = new int[sizeOfArray];
-		int[] unsortedArray2 = new int[sizeOfArray];
 
 		List<Integer> unsortedList = new ArrayList<>();
 		for (int i = 0; i < sizeOfArray; i++) {
-			int randomNumber = random.nextInt(1000);
-			unsortedArray[i] = randomNumber;
-			unsortedArray1[i] = randomNumber;
-			unsortedArray2[i] = randomNumber;
-			unsortedList.add(unsortedArray[i]);
+			int randomNumber = random.nextInt();
+			unsortedList.add(randomNumber);
 		}
-		/*
-		 * Selection Sort
-		 */
-		Sorting selectionSort = new SelectionSort(unsortedArray);
-		long selection_timeTaken_1 = System.currentTimeMillis();
-		int[] selectionSortedArray = selectionSort.sort();
-		long selection_timeTaken_2 = System.currentTimeMillis();
-		System.out.println(
-				selectionSort.getSortingType() + " Time Taken : " + (selection_timeTaken_2 - selection_timeTaken_1));
-		printArray(selectionSortedArray);
 
-		// Insertion Sort
-		Sorting insertionSort = new InsertionSort(unsortedArray1);
-		long insertion_timeTaken_1 = System.currentTimeMillis();
-		int[] insertionSortedArray = insertionSort.sort();
-		long insertion_timeTaken_2 = System.currentTimeMillis();
-		System.out.println(
-				insertionSort.getSortingType() + " Time Taken : " + (insertion_timeTaken_2 - insertion_timeTaken_1));
-		printArray(insertionSortedArray);
-
-		Sorting quickSort = new QuickSort(unsortedArray2);
-		long quick_timeTaken_1 = System.currentTimeMillis();
-		int[] quickSortedArray = quickSort.sort();
-		long quick_timeTaken_2 = System.currentTimeMillis();
-		System.out.println(quickSort.getSortingType() + " Time Taken : " + (quick_timeTaken_2 - quick_timeTaken_1));
-		printArray(quickSortedArray);
-
-		// Bubble Sort
-//		Sorting bubbleSort = new BubbleSort(unsortedArray);
-//		long bubble_timeTaken_1 = System.currentTimeMillis();
-//		int[] bubbleSortedArray = bubbleSort.sort();
-//		long bubble_timeTaken_2 = System.currentTimeMillis();
-//		System.out.println(bubbleSort.getSortingType() + " Time Taken : " + (bubble_timeTaken_2 - bubble_timeTaken_1));
-//		printArray(bubbleSortedArray);
-
-		// Java Sort
-		long time_taken_3 = System.currentTimeMillis();
-		Collections.sort(unsortedList);
-		long time_taken_4 = System.currentTimeMillis();
-		System.out.println("Default  Sort: " + (time_taken_4 - time_taken_3));
-		for (int i : unsortedList) {
-			System.out.print(i + " | ");
-		}
+		// bubbleSort(convertToArray(unsortedList));
+		// insertionSort(convertToArray(unsortedList));
+		// selectionSort(convertToArray(unsortedList));
+		quickSort(convertToArray(unsortedList));
+		defaultSort(unsortedList);
 		scanner.close();
 
 	}
@@ -79,6 +36,68 @@ public class SortingDemo {
 		}
 		System.out.println();
 
+	}
+
+	private static void bubbleSort(int[] unsortedArray) {
+		// Bubble Sort
+		Sorting bubbleSort = new BubbleSort(unsortedArray);
+		long bubble_timeTaken_1 = System.currentTimeMillis();
+		int[] bubbleSortedArray = bubbleSort.sort();
+		long bubble_timeTaken_2 = System.currentTimeMillis();
+		System.out.println(bubbleSort.getSortingType() + " Time Taken : " + (bubble_timeTaken_2 - bubble_timeTaken_1));
+		printArray(bubbleSortedArray);
+	}
+
+	private static void insertionSort(int[] unsortedArray) {
+		// Insertion Sort
+		Sorting insertionSort = new InsertionSort(unsortedArray);
+		long insertion_timeTaken_1 = System.currentTimeMillis();
+		int[] insertionSortedArray = insertionSort.sort();
+		long insertion_timeTaken_2 = System.currentTimeMillis();
+		System.out.println(
+				insertionSort.getSortingType() + " Time Taken : " + (insertion_timeTaken_2 - insertion_timeTaken_1));
+		printArray(insertionSortedArray);
+	}
+
+	private static void selectionSort(int[] unsortedArray) {
+		/*
+		 * Selection Sort
+		 */
+		Sorting selectionSort = new SelectionSort(unsortedArray);
+		long selection_timeTaken_1 = System.currentTimeMillis();
+		int[] selectionSortedArray = selectionSort.sort();
+		long selection_timeTaken_2 = System.currentTimeMillis();
+		System.out.println(
+				selectionSort.getSortingType() + " Time Taken : " + (selection_timeTaken_2 - selection_timeTaken_1));
+		printArray(selectionSortedArray);
+	}
+
+	private static void quickSort(int[] unsortedArray) {
+		Sorting quickSort = new QuickSort(unsortedArray);
+		long quick_timeTaken_1 = System.currentTimeMillis();
+		int[] quickSortedArray = quickSort.sort();
+		long quick_timeTaken_2 = System.currentTimeMillis();
+		System.out.println(quickSort.getSortingType() + " Time Taken : " + (quick_timeTaken_2 - quick_timeTaken_1));
+		printArray(quickSortedArray);
+	}
+
+	private static void defaultSort(List<Integer> unsortedList) {
+		// Java Sort
+		long time_taken_3 = System.currentTimeMillis();
+		Collections.sort(unsortedList);
+		long time_taken_4 = System.currentTimeMillis();
+		System.out.println("Default  Sort: " + (time_taken_4 - time_taken_3));
+		for (int i : unsortedList) {
+			System.out.print(i + " | ");
+		}
+	}
+
+	private static int[] convertToArray(List<Integer> unsortedList) {
+		int[] unsortedArray = new int[unsortedList.size()];
+		for (int i = 0; i < unsortedList.size(); i++) {
+			unsortedArray[i] = unsortedList.get(i);
+		}
+		return unsortedArray;
 	}
 
 }
